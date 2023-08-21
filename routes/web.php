c00b510277fb748382f0cce7c75e071e2adfd870
Aditya Prastika <adityaprastika03@gmail.com>
2023-08-21 11:45:36 +0800

    admin login interface

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\admin\AdminLoginController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login', [AdminLoginController::class,'index'])->name('admin.login');
