15b06b479c4b6eda6688231517bfae0014372621
Aditya Prastika <adityaprastika03@gmail.com>
2023-09-08 11:33:20 +0800

    Committing local changes to app.blade.php

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -177,6 +177,25 @@
 		</div>
 	</div>
 </footer>
+
+<!-- Wishlist Modal -->
+<div class="modal fade" id="wishlistModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h5 class="modal-title" id="exampleModalLabel">Success</h5>
+		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		</div>
+		<div class="modal-body">
+
+		</div>
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		</div>
+	  </div>
+	</div>
+  </div>
+
 <script src="{{ asset('front-assets/js/jquery-3.6.0.min.js') }}"></script>
 <script src="{{ asset('front-assets/js/bootstrap.bundle.5.1.3.min.js') }}"></script>
 <script src="{{ asset('front-assets/js/instantpages.5.1.0.min.js') }}"></script>
@@ -221,6 +240,28 @@ function addToCart(id){
                 }
             });
         }
+
+function addToWishlist(id) {
+	$.ajax({
+                url: '{{ route("front.addToWishlist") }}',
+                type: 'post',
+                data: {id:id},
+                dataType: 'json',
+                success: function(response){
+					console.log(response);
+                    if (response.status == true) {
+                       
+						$("#wishlistModal .modal-body").html(response.message)
+						$("#wishlistModal").modal('show');
+
+                    } else {
+						window.location.href = '{{ route("account.login") }}';
+                        //alert(response.message);
+                    }
+                }
+            });
+}
+
 </script>
 
 @yield('customJs')
