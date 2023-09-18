406630902abf998b28e8541fc3d2e8aedc9f8c18
Aditya Prastika <adityaprastika03@gmail.com>
2023-09-18 16:21:45 +0800

    Midtrans 80.1% (Callback belum berfungsi)

---
+++ app/Services/Midtrans/CreateSnapTokenService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services\Midtrans;
+
+use Midtrans\Snap;
+
+class CreateSnapTokenService extends Midtrans
+{
+    protected $order;
+
+    public function __construct($order)
+    {
+        parent::__construct();
+
+        $this->order = $order;
+    }
+
+    public function getSnapToken()
+    {
+        $params = [
+            'transaction_details' => [
+                'order_id' => $this->order->id,
+                'gross_amount' => $this->order->grand_total,
+            ],
+            'customer_details' => [
+                'first_name' => $this->order->first_name,
+                'last_name' => $this->order->last_name,
+                'email' => $this->order->email,
+                'phone' => $this->order->mobile,
+            ]
+        ];
+
+        $snapToken = Snap::getSnapToken($params);
+
+        return $snapToken;
+    }
+}
--- config/midtrans.php
+++ config/midtrans.php
@@ -4,6 +4,8 @@ return [
     'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
     'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'server_key' => env('MIDTRANS_SERVER_KEY'),
-    'is_production' => env('MIDTRANS_IS_PRODUCTION'),
-    'snap_url' => env('MIDTRANS_SNAP_URL'),
+
+    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+    'is_sanitized' => false,
+    'is_3ds' => false,
 ];
