f131502e064ba3167d6d11a731c9f59fe51b3a9b
Budi <sangputubudiadnyanaaa@gmail.com>
2023-10-13 11:15:39 +0800

    Minor Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\Category::factory(10)->create();
+        $this->call(AdminSeeder::class);
+        $this->call(CountrySeeder::class);
+        
+        // \App\Models\Category::factory(10)->create();
     }
 }
