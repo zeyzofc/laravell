e65bc97191099ff3c82601ec8dc7b54d26040870
Aditya Prastika <adityaprastika03@gmail.com>
2023-10-13 10:55:05 +0800

    Seeder Admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create an admin user
+        User::create([
+            'name' => 'Admin Name',
+            'email' => 'admin@example.com',
+            'phone' => '1234567890',
+            'role' => 2,
+            'status' => 1,
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
