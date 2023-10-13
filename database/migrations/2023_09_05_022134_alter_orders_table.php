edcbccb3eea09d4552b176abf250e4f3da13a30f
Aditya Prastika <adityaprastika03@gmail.com>
2023-10-13 10:37:07 +0800

    Migration

--- database/migrations/2023_09_05_022134_alter_orders_table.php
+++ database/migrations/2023_09_05_022134_alter_orders_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('orders',function(Blueprint $table){
-            $table->dropColumn('payment_status');
             $table->dropColumn('status');
         });
     }
